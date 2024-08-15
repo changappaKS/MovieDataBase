a4b1a5d297a08255ed225ac22924c1c194333ff6
Changappa K S <changappa.ks@robosoftin.com>
2024-08-15 13:08:11 +0530

    Project setup and readme file is added

---
+++ MovieDataBase/MovieDataBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDataBase
+//
+//  Created by Changappa K S on 15/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
